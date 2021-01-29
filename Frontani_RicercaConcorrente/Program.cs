ed0a41be1f8a7b7185c393358e044a077925f201
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-29 08:59:31 +0100

    Add project files.

---
+++ Frontani_RicercaConcorrente/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Frontani_RicercaConcorrente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[100];
+            RiempiArray(n);
+            Console.WriteLine("Inserisci un numero :");
+            int n1 = int.Parse(Console.ReadLine());
+            Thread thread = new Thread(() => RicercaNumeri(n1, n));
+            thread.Start();
+            Console.ReadLine();
+        }
+        public static void RicercaNumeri(int n, int[] array)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (n == array[i])
+                {
+                    Console.WriteLine($"numero Trovato: {array[i]} posizione: {i}");
+                }
+            }
+            Console.WriteLine("ricerca terminata");
+        }
+        private static void RiempiArray(int[] numeri)
+        {
+            Random r = new Random();
+            for (int n = 0; n < 100; n++)
+            {
+                numeri[n] = r.Next(0, 100);
+            }
+        }
+
+
+    
+    }
+}
